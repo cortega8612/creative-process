a0e1a3918609ec76bc3afeff6ea950ad8e684f42
Carlos Ortega <cortega@saic.edu>
2014-06-25 11:46:40 -0500

    slider working, controls still to be figured out

--- js/responsiveslides.js
+++ js/responsiveslides.js
@@ -13,19 +13,19 @@
 
     // Default settings
     var settings = $.extend({
-      "auto": false,             // Boolean: Animate automatically, true or false
+      "auto": true,             // Boolean: Animate automatically, true or false
       "speed": 500,             // Integer: Speed of the transition, in milliseconds
       "timeout": 4000,          // Integer: Time between slide transitions, in milliseconds
       "pager": true,           // Boolean: Show pager, true or false
       "nav": true,             // Boolean: Show navigation, true or false
       "random": false,          // Boolean: Randomize the order of the slides, true or false
-      "pause": true,           // Boolean: Pause on hover, true or false
-      "pauseControls": true,    // Boolean: Pause when hovering controls, true or false
+      "pause": false,           // Boolean: Pause on hover, true or false
+      "pauseControls": false,    // Boolean: Pause when hovering controls, true or false
       "prevText": "Previous",   // String: Text for the "previous" button
       "nextText": "Next",       // String: Text for the "next" button
       "maxwidth": "800",           // Integer: Max-width of the slideshow, in pixels
-      "navContainer": "default",       // Selector: Where auto generated controls should be appended to, default is after the <ul>
-      "manualControls": "custom",     // Selector: Declare custom pager navigation
+      "navContainer": "true",       // Selector: Where auto generated controls should be appended to, default is after the <ul>
+      "manualControls": "true",     // Selector: Declare custom pager navigation
       "namespace": "rslides",   // String: change the default namespace used
       "before": $.noop,         // Function: Before callback
       "after": $.noop           // Function: After callback

d495f1c1e66dbd7206aa71532a1ec44eba2b1983
cortega8612 <cortega@saic.edu>
2014-08-20 16:08:13 -0500

    added Previous and Next to slideshow

--- js/responsiveslides.js
+++ js/responsiveslides.js
@@ -14,18 +14,18 @@
     // Default settings
     var settings = $.extend({
       "auto": true,             // Boolean: Animate automatically, true or false
-      "speed": 2500,             // Integer: Speed of the transition, in milliseconds
-      "timeout": 1000,          // Integer: Time between slide transitions, in milliseconds
-      "pager": true,           // Boolean: Show pager, true or false
-      "nav": true,             // Boolean: Show navigation, true or false
+      "speed": 500,             // Integer: Speed of the transition, in milliseconds
+      "timeout": 4000,          // Integer: Time between slide transitions, in milliseconds
+      "pager": false,           // Boolean: Show pager, true or false
+      "nav": false,             // Boolean: Show navigation, true or false
       "random": false,          // Boolean: Randomize the order of the slides, true or false
       "pause": false,           // Boolean: Pause on hover, true or false
-      "pauseControls": false,    // Boolean: Pause when hovering controls, true or false
+      "pauseControls": true,    // Boolean: Pause when hovering controls, true or false
       "prevText": "Previous",   // String: Text for the "previous" button
       "nextText": "Next",       // String: Text for the "next" button
-      "maxwidth": "800",           // Integer: Max-width of the slideshow, in pixels
-      "navContainer": "wrapper",       // Selector: Where auto generated controls should be appended to, default is after the <ul>
-      "manualControls": "#slider3-pager",     // Selector: Declare custom pager navigation
+      "maxwidth": "",           // Integer: Max-width of the slideshow, in pixels
+      "navContainer": "",       // Selector: Where auto generated controls should be appended to, default is after the <ul>
+      "manualControls": "",     // Selector: Declare custom pager navigation
       "namespace": "rslides",   // String: change the default namespace used
       "before": $.noop,         // Function: Before callback
       "after": $.noop           // Function: After callback
